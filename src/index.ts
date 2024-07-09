ebfe258842c6ff564230ceb6e3f31a8653fecef5
jeyem <me@e-mahmoudi.me>
2024-07-09 11:23:43 +0330

    export default

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { extractResumeFromSections } from "./extract-resume-from-sections";
  *
  * Note: The parser algorithm only works for single column resume in English language
  */
-export const parseResumeFromPdf = async (fileUrl: string) => {
+export default async (fileUrl: string) => {
   // Step 1. Read a pdf resume file into text items to prepare for processing
   const textItems = await readPdf(fileUrl);
   // Step 2. Group text items into lines
